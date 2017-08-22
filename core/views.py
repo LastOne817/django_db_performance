133a473e77e3de9647ae48dbef558a8b9e6e6145
lastone817 <lastone817@gmail.com>
2017-08-22 14:32:08 +0900

    apply memcached

--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
 from django.http import JsonResponse
 from django.db.models import Count, Max
 from .models import Sample
+from django.views.decorators.cache import cache_page
 
 
+@cache_page(60 * 15)
 def index(request):
     l = Sample.objects.filter(content__startswith='A')
     return JsonResponse(list(l.values('content')), safe=False)
