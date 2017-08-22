df331ea636dab177bc3cbb575cee9071cc04048b
lastone817 <lastone817@gmail.com>
2017-08-22 11:59:40 +0900

    add redis cache

--- core/views.py
+++ core/views.py
@@ -2,7 +2,14 @@ from django.http import JsonResponse
 from django.db.models import Count, Max
 from .models import Sample
 
+from django.conf import settings
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from django.views.decorators.cache import cache_page
 
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
+
+
+@cache_page(CACHE_TTL)
 def index(request):
     l = Sample.objects.filter(content__startswith='A')
     return JsonResponse(list(l.values('content')), safe=False)
